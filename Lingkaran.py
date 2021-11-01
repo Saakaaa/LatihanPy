48fd5452430d862bf74bb463b005343d605f357f
Saakaaa <sakanabhan@gmail.com>
2021-11-01 19:00:40 +0700

    Menambahkan File

---
+++ Lingkaran.py
@@ -0,0 +1,7 @@
+import math
+r =float(input("Masukkan Jari-Jari : "))
+phi = 3.14
+luas = phi*(r*r)
+keliling = 2*phi*r
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran \t=",keliling)
